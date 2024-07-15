0fe87aa4bb8ffe367dc43a42051e4e7f9cff1efc
Your Name <you@example.com>
2024-07-14 20:15:16 -0400

    backup

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -30,14 +30,12 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.json({books: books});
+public_users.get('/', async function (req, res) {
+  res.json({books: books});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-  
+public_users.get('/isbn/:isbn', async function (req, res) {
     const book_list = books.filter((book) => {
         return book.isbn == req.params.isbn
     })  
@@ -48,11 +46,11 @@ public_users.get('/isbn/:isbn', function (req, res) {
         book = book_list[0]
     }
 
-    return res.status(300).json(book);
+    res.status(300).json(book);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const book_list = books.filter((book) => {
         return book.author == req.params.author
     })  
@@ -82,7 +80,7 @@ public_users.get('/review/:isbn', function (req, res) {
  });
 
  //  Get book title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
     const book_list = books.filter((book) => {
         return book.title == req.params.title
     })  
@@ -93,7 +91,7 @@ public_users.get('/title/:title',function (req, res) {
         book = book_list[0]
     }
 
-    return res.status(300).json(book);
+    res.status(300).json(book);
  });
 
 
